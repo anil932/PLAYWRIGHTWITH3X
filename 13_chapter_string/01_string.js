798d0ace6dbe91c14391fcb9b70783feded4a38c
Anil Yadav <anil932@gmail.com>
2026-08-10 09:07:12 +0530

    Add string chapter lab exercises
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ 13_chapter_string/01_string.js
@@ -0,0 +1,12 @@
+//  we can sau that string is a sequence of character
+//
+
+console.log("A".charCodeAt(0)) // it will return the ascii code value
+console.log("a".charCodeAt(0))
+
+// charAt() will work for both positive and negative value
+// it will tell the index of the character
+
+console.log("Anil".charAt('2'))
+
+
---
+++ 13_chapter_string/02_stringmethod.js
@@ -0,0 +1,16 @@
+// string inbuld method in js
+
+let str = "Anil Kumar Yadav"
+
+console.log(str.lastIndexOf('a'))
+
+
+let str1 = "Javascript"
+console.log(str1.slice(0,4))
+console.log(str1.slice(4))
+
+
+console.log("a1b22c".split(/\d+/))
+
+console.log("xyz".split("").reverse().join("")). // zyx
+console.log("anil".split("").reverse().join("")) // lina
